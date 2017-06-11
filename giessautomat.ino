648f489bd703b011662decb5530a448160a44783
Justus Beyer <mail@justus-beyer.de>
2017-06-11 22:52:36 +0200

    initial commit

---
+++ giessautomat.ino
@@ -0,0 +1,15 @@
+// Configuration
+const uint8_t PIN=4;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(PIN, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(PIN, HIGH);
+  delay(1000);
+  digitalWrite(PIN, LOW);
+  delay(5000);
+}
